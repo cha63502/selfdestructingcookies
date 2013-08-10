235ddd84529806284e8637550054994a81001aad
Nolan Roberson <nolan@roberson.co>
2013-08-10 03:30:10 -0400

    implemented regex to pull netloc from URL

--- bg.js
+++ bg.js
@@ -5,7 +5,15 @@ function addHostToList(host, list)
 {
 	if (0 > list.indexOf(host))
 	{
-		list.push(host);
+		var prePushLen = list.length;
+		if (list.push(host) > prePushLen)
+		{
+			console.log("added " + host);
+		} else
+		{
+			console.warning("Failed to add " + host);
+		}
+
 	}
 
 }
@@ -19,9 +27,17 @@ function removeHostFromList(host, list)
 	}
 }
 
+function extractNetLoc(url)
+{
+	var re = new RegExp("^.*://([\\d\\w\\.-]+\\.[a-z]{2,6})");
+	var match = re.exec(url);
+	return match[1];
+}
+
 function setupListeners() 
 {
-	addUrlToList("http://google.com");
+	addHostToList(extractNetLoc("http://www.google.com/test"), hosts);
+	console.log(hosts);
 }
 
 console.log("Adding listeners.");
